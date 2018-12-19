bdb0d1bbf375440400fb88a933e432d8acf1ccbf
Jimyung Park <pzy51992@hotmail.com>
2018-12-19 03:31:01 +0000

    add foreign id to famous people table

--- migrations/20181219031016_add_famous_person_id_to_milestones.js
+++ migrations/20181219031016_add_famous_person_id_to_milestones.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.table('famous_people_table', (table) => {
+    table.integer('famous_person_id').unsigned();
+    table.foreign('famous_person_id').references('famous_people.id').onDelete('CASCADE');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.table('urls', (table) => {
+    table.dropColumn('famous_person_id');
+  });
 };
